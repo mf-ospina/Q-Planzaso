469e26546a87813085b7f537eafbfa640c3c18cf
mf-ospina <mafe.salinas2040@gmail.com>
2025-10-06 19:28:16 -0500

    first commit

---
+++ app/src/main/java/com/planapp/qplanzaso/MyApp.kt
@@ -0,0 +1,28 @@
+package com.planapp.qplanzaso
+
+import android.app.Application
+import com.google.firebase.BuildConfig
+import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
+
+class MyApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        FirebaseApp.initializeApp(this)
+
+        // Conecta al emulador de Firebase cuando estás en modo debug
+        if (BuildConfig.DEBUG) {
+            val firestore = FirebaseFirestore.getInstance()
+            firestore.useEmulator("10.0.2.2", 8080)
+
+            val auth = FirebaseAuth.getInstance()
+            auth.useEmulator("10.0.2.2", 9099)
+
+            val storage = Firebase.storage
+            storage.useEmulator("10.0.2.2", 9199)
+        }
+    }
+}
