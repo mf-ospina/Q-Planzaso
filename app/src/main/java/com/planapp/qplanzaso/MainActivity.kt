c5a1be4428047867a0136df9d57f3e362d3b07cc
mf-ospina <mafe.salinas2040@gmail.com>
2025-10-06 20:15:59 -0500

    prueba de conexión a firebase exitosa

--- app/src/main/java/com/planapp/qplanzaso/MainActivity.kt
+++ app/src/main/java/com/planapp/qplanzaso/MainActivity.kt
@@ -12,12 +12,45 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.google.firebase.firestore.FirebaseFirestore
 import com.planapp.qplanzaso.ui.theme.QPlanzasoTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.d("MyApp", "Firebase inicializado correctamente✅")
+
+        //VERIFICACIÓN DE CONEXIÓN A FIREBASE
+        val db = FirebaseFirestore.getInstance()
+
+        // Escribir un documento de prueba
+        val testData = hashMapOf(
+            "nombre" to "Kevin",
+            "mensaje" to "Conexión con Firestore exitosa 🚀"
+        )
+
+        db.collection("pruebas")
+            .add(testData)
+            .addOnSuccessListener { documentReference ->
+                Log.d("FirestoreTest", "Documento agregado con ID: ${documentReference.id}")
+            }
+            .addOnFailureListener { e ->
+                Log.w("FirestoreTest", "Error al agregar documento", e)
+            }
+
+        // Leer todos los documentos de la colección "pruebas"
+        db.collection("pruebas")
+            .get()
+            .addOnSuccessListener { result ->
+                for (document in result) {
+                    Log.d("FirestoreTest", "${document.id} => ${document.data}")
+                }
+            }
+            .addOnFailureListener { e ->
+                Log.w("FirestoreTest", "Error al leer documentos", e)
+            }
+
+
         enableEdgeToEdge()
         setContent {
             QPlanzasoTheme {
